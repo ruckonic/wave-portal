ef9a8dfb7b7c5b43f49a26bd0ff80e48f8973624
ruckonic <ruckonic@icloud.com>
2021-10-16 14:53:46 -0600

    feat: use contract

---
+++ scripts/run.js
@@ -0,0 +1,17 @@
+const hre = require('hardhat')
+
+async function main() {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
+  const waveContract = await waveContractFactory.deploy()
+
+  await waveContract.deployed()
+
+  console.log('Contract deployed to:', waveContract.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
