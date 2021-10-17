b70068771931a0711dcd92dbf2445fe3327ebd41
ruckonic <ruckonic@icloud.com>
2021-10-17 17:33:10 -0600

    feat: depluy

--- scripts/run.js
+++ scripts/run.js
@@ -1,29 +1,17 @@
 const hre = require('hardhat')
 
 async function main() {
-  const [owner, randomPerson] = await hre.ethers.getSigners()
-  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
-  const waveContract = await waveContractFactory.deploy()
+  const [owner] = await hre.ethers.getSigners()
+  const ownerBalance = await owner.getBalance()
 
-  await waveContract.deployed()
+  console.log('Deployed with account: ', owner.address)
+  console.log('Owner balance: ', ownerBalance.toString())
 
-  console.log('Contract deployed to:', waveContract.address)
-  console.log('Contract deployed by:', owner.address)
+  const Contract = await hre.ethers.getContractFactory('WavePortal')
+  const portal = await Contract.deploy()
+  await portal.deployed()
 
-  let waveCount = await waveContract.getTotalWaves()
-
-  let waveTxt = await waveContract.wave()
-  await waveTxt.wait()
-
-  waveCount = await waveContract.getTotalWaves()
-
-  const waveTxt2 = await waveContract.connect(randomPerson).wave()
-  const waveTxt3 = await waveContract.connect(randomPerson).wave()
-
-  await waveTxt2.wait()
-  await waveTxt3.wait()
-
-  waveCount = await waveContract.connect(randomPerson).getTotalWaves()
+  console.log('WavePortal address: ', portal.address)
 }
 
 main()
